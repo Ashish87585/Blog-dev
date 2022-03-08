938b1dbe9e12f1f8bfd0f85410f85adaef69c705
Ashish Vekariya <ashishvekariya494@gmail.com>
2022-03-08 17:37:07 +0530

    Home page blog list done

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Loading from '../components/global/Loading';
+import CardVert from '../components/cards/CardVert';
+
+import { RootStore } from '../utils/TypeScript';
 
 const Home = () => {
+  const { homeBlogsReducer } = useSelector((state: RootStore) => state);
+
+  if (homeBlogsReducer.length === 0) return <Loading />;
   return (
-    <div>Home</div>
-  )
-}
+    <div className='home_page'>
+      {homeBlogsReducer.map((homeBlog) => (
+        <div key={homeBlog._id}>
+          {homeBlog.count > 0 && (
+            <>
+              <h3>
+                <Link to={`/blogs/${homeBlog.name.toLowerCase()}`}>
+                  {homeBlog.name} <small>({homeBlog.count})</small>
+                </Link>
+              </h3>
+              <hr className='mt-1' />
+
+              <div className='home_blogs'>
+                {homeBlog.blogs.map((blog) => (
+                  <CardVert key={blog._id} blog={blog} />
+                ))}
+              </div>
+            </>
+          )}
+
+          {homeBlog.count > 3 && (
+            <Link
+              className='text-end d-block mt-2 mb-3'
+              to={`/blogs/${homeBlog.name}`}>
+              Read more &gt;&gt;
+            </Link>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Home
