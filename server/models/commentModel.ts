981a72f51d27dca9a246eb0582b14ee392ac4b93
Ashish Vekariya <ashishvekariya494@gmail.com>
2022-03-09 18:01:28 +0530

    Create commit backend omplete

---
+++ server/models/commentModel.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+import { IComment } from '../config/interface'
+
+
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    blog_id: mongoose.Types.ObjectId,
+    blog_user_id: mongoose.Types.ObjectId,
+    content: {
+        type: String,
+        required: true
+    },
+    replyCM: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ],
+    reply_user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+}, {
+    timestamps: true
+})
+
+
+export default mongoose.model<IComment>('comment', commentSchema)
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,12 +2,14 @@ import authRouter from  './authRoute';
 import userRouter from './userRoute'
 import categoryRouter from './categoryRoute'
 import blogRouter from './blogRoute'
+import commentRouter from './commentRoute'
 
 const routes = {
     authRouter,
     userRouter,
     categoryRouter,
-    blogRouter
+    blogRouter,
+    commentRouter
 }
 
 export default routes;
