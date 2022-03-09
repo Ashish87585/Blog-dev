e1cb7fde6160be29099599dd6948840947956134
Ashish Vekariya <ashishvekariya494@gmail.com>
2022-03-09 18:01:57 +0530

    route add

--- server/index.ts
+++ server/index.ts
@@ -26,7 +26,8 @@ app.use('/api', routes.authRouter);
 app.use('/api', routes.userRouter);
 app.use('/api', routes.categoryRouter);
 app.use('/api', routes.blogRouter);
-app.use('/api', routes.commentRouter);
+app.use('/api', routes.commentRouter
+);
 
 //Database
 import './config/database'
